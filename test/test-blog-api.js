1cb80f3a81c877a51d7ae30375f19262d0f5a92e
condericson@gmail.com <condericson@gmail.com>
2017-01-27 21:32:14 -0500

    fixed status number

--- test/test-blog-api.js
+++ test/test-blog-api.js
@@ -64,7 +64,7 @@ describe('Blog Posts', function() {
       .send(updateData);
     })
     .then(function(res) {
-      res.should.have.status(200);
+      res.should.have.status(201);
       res.should.be.json;
       res.body.should.be.a('object');
       res.body.should.deep.equal(updateData);
